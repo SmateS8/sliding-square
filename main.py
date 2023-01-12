b8518a08659c75548e34309b94b778b2dc188e3d
Matyáš Sýs <sysm22@spstrutnov.cz>
2023-01-12 19:06:44 +0100

    Added player movement
    Completely functional, but feels werid

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ P_HEIGHT = 20
 P_COLOR = (255,0,0)
 Player = pygame.Rect(WIDTH/2 - P_WIDTH/2, HEIGHT/2 - P_HEIGHT/2 ,P_WIDTH, P_HEIGHT) # Sets Rectangle in the middle of the screen
 MAX_VEL = 3 # Maximum velocity
-SLIDE = 0.025  # Is the deceleration while sliding the square
-ACCEL = 0.1 # Is the acceleration of the square
+SLIDE = 0.05  # Is the deceleration while sliding the square
+ACCEL = 0.2 # Is the acceleration of the square
 
 speed = [0,0] # X speed, Y speed
 
@@ -70,7 +70,33 @@ def update_player_velocity(pressed):
                 else:
                     speed[0] = 0  
 
-    print(speed)    
+# Handles the actual square movement
+def move_player(speed):
+    global WIDTH, HEIGHT
+    #handle x axis
+    if speed[0] > 0:
+        if Player.x + speed[0] <= WIDTH - P_WIDTH:
+            Player.x +=speed[0]
+        else:
+            Player.x = WIDTH - P_WIDTH
+    if speed[0] < 0:
+        if Player.x + speed[0] >= 0:  #* Speed can be negative, that is why there is plus, not minus
+            Player.x +=speed[0]
+        else:
+            Player.x = 0
+    # handle y axis
+
+    if speed[1] > 0:
+        if Player.y + speed[1] <= HEIGHT - P_HEIGHT:
+            Player.y +=speed[1]
+        else:
+            Player.y = HEIGHT - P_HEIGHT
+    if speed[1] < 0:
+        if Player.y + speed[1] >= 0:  #* Speed can be negative, that is why there is plus, not minus
+            Player.y +=speed[1]
+        else:
+            Player.y = 0
+
     
 
 
@@ -94,6 +120,9 @@ def main():
         # ...
 
         # Draw the screen
+        speed[0] = round(speed[0],2)
+        speed[1] = round(speed[1],2)
+        move_player(speed)
         WIN.fill((0, 0, 0))
         pygame.draw.rect(WIN, P_COLOR, Player)
         pygame.display.flip()
